634319091cc06fa698db1dec8a6b512d8f3c437a
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-10 12:17:26 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 def prime?(number) 
   if number > 2
-  (2..number-1).to_a.all? { |x|
-  number % x != 0 
-  }
+    (2..number-1).to_a.all? { |x|
+    number % x != 0 
+    }
   else 
     return false
   end
